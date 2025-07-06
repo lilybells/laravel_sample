007803491c8f99e1c730dd0e525aaabb65a1426f
ringo <ringo@ringonoMacBook-Pro.local>
2025-07-07 04:00:07 +0900

    ヘッダーの修正

--- src/resources/views/layouts/header.blade.php
+++ src/resources/views/layouts/header.blade.php
@@ -7,10 +7,6 @@
         <nav>
             <ul class="nav">
                 <li><a href="/">ホーム</a></li>
-                <li><a href="/welcome">Welcome</a></li>
-                <li><a href="http://localhost:8080" target="_blank">phpMyAdmin</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
             </ul>
         </nav>
     </div>
